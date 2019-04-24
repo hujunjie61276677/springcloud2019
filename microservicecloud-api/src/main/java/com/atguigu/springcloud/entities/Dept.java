1db8e8302eeeee59565b09428b382a14f3604ef9
hujunjie61276677 <947582631@qq.com>
2019-04-24 14:54:33 +0800

    测试图形化  新建分支的代码提交

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -17,7 +17,7 @@ import lombok.experimental.Accessors;
 public class Dept implements Serializable// entity --orm--- db_table
 {
 	private Long 	deptno; // 主键测试Git本地库的位置
-	private String 	dname; // 部门名称
+	private String 	dname; // 部门名称测试图形化新建分支
 	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 	
 	public Dept(String dname)
